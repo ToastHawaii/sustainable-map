4f5813c9f3507f57d674cf2f7069dd1f47dbf15d
ToastHawaii <mar.pel@protonmail.com>
2020-06-16 23:11:54 +0200

    Add sharing button

--- src/createOverPassLayer.ts
+++ src/createOverPassLayer.ts
@@ -110,13 +110,13 @@ export function createOverPassLayer<M>(
         const contentElement = createElement(
           "div",
           `<div id="hcard-Name" class="vcard">
-          <a style="float:right;" href="https://www.openstreetmap.org/edit?${
+          <a style="float:right;margin-left: 5px;" href="https://www.openstreetmap.org/edit?${
             e.type
-          }=${
-            e.id
-          }"><i class="fas fa-pencil-alt"></i></a><strong class="name" title="${toTitle(
+          }=${e.id}"><i class="fas fa-pencil-alt"></i></a>
+          <a style="float:right;" href="" class="share"><i class="fas fa-share-alt"></i></a>
+          <strong class="name" title="${toTitle(model)}">${toTitle(
             model
-          )}">${toTitle(model)}</strong>
+          )}</strong>
         <div class="adr">
 
         ${attributesGenerator.render(local, e.tags, value, {} as M)}
@@ -199,6 +199,47 @@ export function createOverPassLayer<M>(
         </div>
         </div>`
         );
+        const share = contentElement.querySelector(".share") as HTMLLinkElement;
+        share.addEventListener("click", function (e) {
+          e.preventDefault();
+          const data = {
+            url: window.location.href,
+            title: toTitle(model),
+            text:
+              model.description ||
+              model.wikipediaDescription ||
+              model.wikimediaDescription
+          };
+          if (
+            (navigator as any).canShare &&
+            (navigator as any).canShare(data) &&
+            (navigator as any).share
+          ) {
+            (navigator as any)
+              .share(data)
+              .then(() => console.log("Share was successful."))
+              .catch((error: any) => console.log("Sharing failed", error));
+          } else {
+            console.log(
+              `Your system doesn't support sharing files. Use copy to clipboard.`
+            );
+            copyTextToClipboard(window.location.href);
+
+            const target = (e.target as HTMLElement).parentElement;
+
+            if (target) {
+              const titleElement = createElement("span", local.linkCopied, [
+                "title"
+              ]);
+
+              target.append(titleElement);
+
+              setTimeout(() => {
+                titleElement.remove();
+              }, 2000);
+            }
+          }
+        });
         const popup = L.popup({
           minWidth: 200,
           autoPanPaddingTopLeft: [10, 85],
@@ -478,4 +519,61 @@ async function loadWikipediaSummary(siteTitle: string, language: string) {
   } else {
     throw new Error("No summary found");
   }
+}
+
+function copyTextToClipboard(text: string) {
+  const textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = "fixed";
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = "2em";
+  textArea.style.height = "2em";
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = "0";
+
+  // Clean up any borders.
+  textArea.style.border = "none";
+  textArea.style.outline = "none";
+  textArea.style.boxShadow = "none";
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = "transparent";
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    const successful = document.execCommand("copy");
+    const msg = successful ? "successful" : "unsuccessful";
+    console.log("Copying text command was " + msg);
+  } catch (err) {
+    console.log("Oops, unable to copy");
+  }
+
+  document.body.removeChild(textArea);
 }
