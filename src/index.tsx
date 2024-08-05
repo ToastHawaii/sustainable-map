7ad2dccec23007ded9be0b7df81fd7f33b4ef9c9
ToastHawaii <mar.pel@protonmail.com>
2024-08-05 21:44:21 +0200

    move to peace flag

--- src/index.tsx
+++ src/index.tsx
@@ -202,3 +202,13 @@ root.render(
     />
   </React.StrictMode>
 );
+
+// change to peace flag
+setTimeout(() => {
+  const flag = document.querySelector(".leaflet-attribution-flag");
+  if (flag) {
+    flag.setAttribute("viewBox", "0 0 21 14");
+    flag.innerHTML = `<path fill="#80b" d="M0 0h21v3H0z"/><path fill="#00f" d="M0 2h21v3H0z"/><path fill="#8bd" d="M0 4h21v3H0z"/><path fill="#6b4" d="M0 6h21v3H0z"/><path fill="#fe0" d="M0 8h21v3H0z"/><path fill="#f41" d="M0 10h21v3H0z"/><path fill="#f10" d="M0 12h21v2H0z"/>`;
+  flag.after(" | ")
+  }
+}, 0);
