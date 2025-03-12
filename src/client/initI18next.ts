1f3eaf70d6da83e7cd1eaf9cac7da72d690ac60b
ToastHawaii <mar.pel@protonmail.com>
2025-03-12 19:19:09 +0100

    add support for more languages

--- src/client/initI18next.ts
+++ src/client/initI18next.ts
@@ -11,8 +11,14 @@ import * as es from "./locales/es.json";
 import * as oacEs from "../osm-app-component/locales/es.json";
 import * as fr from "./locales/fr.json";
 import * as oacFr from "../osm-app-component/locales/fr.json";
+import * as hu from "./locales/hu.json";
+import * as oacHu from "../osm-app-component/locales/hu.json";
 import * as pl from "./locales/pl.json";
 import * as oacPl from "../osm-app-component/locales/pl.json";
+import * as ru from "./locales/ru.json";
+import * as oacRu from "../osm-app-component/locales/ru.json";
+import * as ta from "./locales/ta.json";
+import * as oacTa from "../osm-app-component/locales/ta.json";
 import * as zhHant from "./locales/zh_Hant.json";
 import * as oacZhHant from "../osm-app-component/locales/zh-Hant.json";
 
@@ -22,7 +28,10 @@ export const Resources = {
   de: { translation: { ...de, ...oacDe } },
   es: { translation: { ...es, ...oacEs } },
   fr: { translation: { ...fr, ...oacFr } },
+  hu: { translation: { ...hu, ...oacHu } },
   pl: { translation: { ...pl, ...oacPl } },
+  ru: { translation: { ...ru, ...oacRu } },
+  ta: { translation: { ...ta, ...oacTa } },
   zh_Hant: { translation: { ...zhHant, ...oacZhHant } },
 };
 
